9f83a69dad98411cc59b3e256a734dc7ffd1d1d2
C W <wahlers1@gmail.com>
2017-04-30 08:21:09 -0400

    Add Mobile logs

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var cookieParser =  require("cookie-parser");
 var session = require("express-session");
 var port = process.env.PORT || 3000;
 var app = module.exports = express(); 
+var json = require('jsonify');
+
+//For Mobile Connection
+var mysql = require('mysql')
+var connection = require('./config/connection.js')
 
 //allow the use of sessions
 app.use(session({ secret: 'app', cookie: { maxAge: 6*1000*1000*1000*1000*1000*1000 }}));
@@ -35,5 +40,17 @@ app.use("/", applicationController);
 app.use("/users", usersController);
 app.use("/readers", readersController);
 app.use("/parents", parentsController);
+
+//Mobile routes
+  app.get("/mobile/logs/:id", function(req, res) {
+    console.log("Mobile get");
+    console.log("id: " + req.params.id);
+
+    var query = "SELECT l.created, l.time_lapsed, b.title , DATE_FORMAT(l.created, '%d/%m/%Y') AS 'log_created' FROM logs l LEFT JOIN books b ON l.book_id = b.id WHERE user_id = ?";
+    connection.query(query, [ req.params.id ], function(err, response){
+      //console.log(response);
+      res.json(response);
+    });
+  });
 
 app.listen(port);
