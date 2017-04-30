d1a5762701c488f13ed1e904b387a3327ab97856
C W <wahlers1@gmail.com>
2017-04-30 09:48:49 -0400

    Add books

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use("/parents", parentsController);
 
 //Mobile routes
   app.get("/mobile/logs/:id", function(req, res) {
-    console.log("Mobile get");
+    console.log("Mobile get logs");
     console.log("id: " + req.params.id);
 
     var query = "SELECT l.created, l.time_lapsed, b.title , DATE_FORMAT(l.created, '%d/%m/%Y') AS 'log_created' FROM logs l LEFT JOIN books b ON l.book_id = b.id WHERE user_id = ?";
@@ -51,6 +51,62 @@ app.use("/parents", parentsController);
       //console.log(response);
       res.json(response);
     });
+  });
+
+  app.get("/mobile/books/:id", function(req, res) {
+    console.log("Mobile get books");
+    console.log("id: " + req.params.id);
+
+    var query = "SELECT ub.book_id, b.title, b.author, ub.current_page FROM user_books AS ub LEFT JOIN books AS b ON ub.book_id = b.id WHERE ub.user_id = ?";
+    connection.query(query, [ req.params.id ], function(err, response){
+      //console.log(response);
+      res.json(response);
+    });
+  });
+
+   app.post("/mobile/login", function(req, res) {
+    console.log("Mobile post login");
+
+    var query = "SELECT * FROM users WHERE email = ?";
+
+    connection.query(query, [ req.body.email ], function(err, response) {
+      if (response.length == 0){
+        res.redirect('/users/sign-in')
+      }
+
+        bcrypt.compare(req.body.password, response[0].password_hash, function(err, result) {
+            if (result == true){
+              console.log("Logged into the app");
+              req.session.logged_in = true;
+              req.session.user_id = response[0].id;
+              req.session.user_email = response[0].email;
+              req.session.usertype = response[0].usertype;
+              req.session.username = response[0].username;
+
+              console.log("Usertype: " + response[0].usertype );
+              switch (response[0].usertype){
+                  case 'R':
+                    req.session.is_reader = true;
+                    req.session.is_parent = false;
+                    res.redirect('/readers');
+                    break;
+                  case 'P':
+                    req.session.is_reader = false;
+                    req.session.is_parent = true;
+                    res.redirect('/parents');
+                    break;
+                  case 'T':
+                    res.redirect('/readers');
+                    break;
+                }
+
+            }else{
+              res.redirect('/users/sign-in')
+            }
+      });
+  });
+
+
   });
 
 app.listen(port);
