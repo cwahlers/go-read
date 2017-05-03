7ec51741a3b3d9444d0d7f6afb551a20f8eee6f9
C W <wahlers1@gmail.com>
2017-05-03 17:11:03 -0400

    Adding CORS

--- server.js
+++ server.js
@@ -42,6 +42,21 @@ app.use("/users", usersController);
 app.use("/readers", readersController);
 app.use("/parents", parentsController);
 
+
+/* if we don't do this here then we'll get this error in apps that use this api
+
+  Fetch API cannot load No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin is therefore not allowed access. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+
+  read up on CORs here: https://www.maxcdn.com/one/visual-glossary/cors/
+*/
+  //allow the api to be accessed by other apps
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    next();
+  });
+
 //Mobile routes
   app.get("/mobile/logs/:id", function(req, res) {
     console.log("Mobile get logs");
